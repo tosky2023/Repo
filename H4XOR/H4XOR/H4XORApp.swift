054b98d058618d816a05b01d92a5f66ae8de163f
“tosky2023” <panhequn007@gmail.com>
2023-04-09 16:19:58 +0800

    tsp

---
+++ H4XOR/H4XOR/H4XORApp.swift
@@ -0,0 +1,18 @@
+//
+//  H4XORApp.swift
+//  H4XOR
+//
+//  Created by 潘鹤群 on 2023/4/6.
+//
+
+import SwiftUI
+
+@main
+struct H4XORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+asldjflajsdfl;a;sldfjl
